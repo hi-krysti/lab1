83fda8a0b9114c0f94845993815e4afb79f8f6e3
Krysti <hi.krysti@gmail.com>
2023-10-04 21:56:21 -0400

    submission

---
+++ lab1.py
@@ -0,0 +1,29 @@
+# Exercise 1.1: Echo String
+text = input("Enter some text: ")
+print(text)
+
+# Exercise 1.2: Adding a number to an integer
+number = input("Enter a number: ")
+print(int(number) + 1)
+
+# Exercise 1.3: Adding a number to a float
+number = input("Enter a number: ")
+print(float(number) + .5)
+
+# Exercise 1.4: Adding two floats
+number = input("Enter a number: ")
+number_two = input("Enter another number: ")
+total = float(number) + float(number_two)
+print("The sum is " + str(total))
+
+# Exercise 1.5: Multiplying Floats
+number = input("Enter a number: ")
+number_two = input("Enter another number: ")
+total = float(number) * float(number_two)
+print("The product is " + str(total))
+
+# Exercise 1.6: Dividing Integers
+number = input("Enter a number: ")
+number_two = input("Enter another number: ")
+total = int(number) // int(number_two)
+print("The result is " + str(total))
